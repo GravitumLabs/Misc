fb1a84fee9c3db3a08fb48b3d96a80b8b12798f8
team@gravitum.com <team@gravitum.com>
2017-11-27 17:02:46 +0400

    HB

--- app.py
+++ app.py
@@ -1,92 +1,94 @@
-from flask import Flask, request,render_template, url_for,jsonify,make_response
-from flask_api import status
 import os
-from gravitumUtils import *
+import sys
+import json
+from datetime import datetime
+
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def get_webhook():
-	# when the endpoint is registered as a webhook, it must echo back
-	# the 'hub.challenge' value it receives in the query arguments
-	# if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-	# 	if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-	# 		return "Verification token mismatch", 403
-	# 	return request.args["hub.challenge"], 200
-	data = request.get_json()
-	logJsn(data)
-	return "Hello world. I am alive and well.", 200
+def verify():
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
 
-# @app.route('/api/',methods=['GET'])
-# def apiGetProcessor():
-#
-# 	log("--------- API GET Request Received : {function} --------- ".format(function=request.args.get ( "function" )))
-# 	user_agent = request.user_agent
-# 	result = jsonify ( '' ) , status.HTTP_400_BAD_REQUEST
-# 	fnctName=fnctArgs =fnctResult= None
-# 	if 'function' in request.args:
-# 		#Call the wrapper function
-# 		fnctResult=apiProcessor(request.args,user_agent)
-# 		#Jsonify the result from Python to send to the webview
-# 		if fnctResult is not None:
-# 			result = jsonify(fnctResult)
-# 			log ( "Successful GET....(200)" )
-# 			try :
-# 				result = gzipper ( response = result )
-# 			except Exception as e :
-# 				log ( "GZIPPER FAILED" )
-# 				log ( str ( e ) )
-# 		else:
-# 			log ( "ERROR IN GET....(400)" )
-# 			result = jsonify ( '' ) , status.HTTP_400_BAD_REQUEST
-#
-#
-# 	return result
-#
-#
-# @app.route ( '/api/' , methods = [ 'POST' ] )
-# def apiPostComms ( ) :
-# 	result = None
-#
-# 	data = request.data
-# 	dataDict = json.loads ( data ,encoding = 'utf-8')
-# 	user_agent=request.user_agent
-#
-#
-# 	if (data is not None) or (data != ''):
-# 		#Insert user_agent into data
-# 		dataDict["user_agent"]=user_agent
-#
-#
-# 		log("--------- API POST Request Received : {post_type}--------- ".format(post_type=safeGetFromDict(dataDict,["postType"])))
-# 		s="POST Data: "+str(data)[:1024]
-# 		log(s)
-# 		[ POSTResult , POSTError ]=apiPOSTProcessor(dataDict)
-#
-# 		if not POSTError:
-# 			result = jsonify ( str(POSTResult)) , status.HTTP_200_OK
-# 			log("Successful POST....(200)")
-# 		else :
-# 			result=jsonify(str(POSTResult)) , status.HTTP_400_BAD_REQUEST
-# 			log ( "ERROR IN POST....(400)" )
-#
-# 	return result
-
-
-
+    return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
-def post_webhook():
-	data = request.get_json()
-	logJsn(data)
-	result = jsonify("Post Received ok", status.HTTP_200_OK)
-	return result
+def webhook():
+
+    # endpoint for processing incoming messaging events
+
+    data = request.get_json()
+    log(data)  # you may not want to log every incoming message in production, but it's good for testing
+
+    if data["object"] == "page":
+
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+
+                if messaging_event.get("message"):  # someone sent us a message
+
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message_text = messaging_event["message"]["text"]  # the message's text
+
+                    send_message(sender_id, "roger that!")
+
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
+
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
+
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    pass
+
+    return "ok", 200
 
 
+def send_message(recipient_id, message_text):
+
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = json.dumps({
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    })
+    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    if r.status_code != 200:
+        log(r.status_code)
+        log(r.text)
 
 
+def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+    try:
+        if type(msg) is dict:
+            msg = json.dumps(msg)
+        else:
+            msg = unicode(msg).format(*args, **kwargs)
+        print u"{}: {}".format(datetime.now(), msg)
+    except UnicodeEncodeError:
+        pass  # squash logging errors in case of non-ascii text
+    sys.stdout.flush()
 
 
-
+if __name__ == '__main__':
+    app.run(debug=True)
